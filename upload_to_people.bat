e9ce80b7f8b094a2f0522a214394bdc20dc179ef
kyle lahnakoski <klahnakoski@mozilla.com>
2014-04-05 22:54:04 +0000

    scripts that work

--- upload_to_people.bat
+++ upload_to_people.bat
@@ -1,2 +1,5 @@
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" klahnakoski@people.mozilla.com /script="C:\Users\klahnakoski\scripts\upload_to_people.script" /log=upload_to_people.log
+SET PYTHONPATH=.
+python current_time.py
+
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" klahnakoski@people.mozilla.com /script="upload_to_people.script" /log=upload_to_people.log
 type upload_to_people.log
