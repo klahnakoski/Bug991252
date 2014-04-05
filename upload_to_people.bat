4cde96a11ca08fd63093654ec4eb89845b0bcef9
kyle lahnakoski <klahnakoski@mozilla.com>
2014-04-05 22:38:46 +0000

    scripts for uploading

---
+++ upload_to_people.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" klahnakoski@people.mozilla.com /script="C:\Users\klahnakoski\scripts\upload_to_people.script" /log=upload_to_people.log
+type upload_to_people.log
