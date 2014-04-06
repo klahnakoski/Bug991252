a7d58df34f2574e8ce8cd2bb1e837f27a039aff3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-04-05 22:52:52 -0400

    Finally can trigger the bfcache

--- current_time.py
+++ current_time.py
@@ -11,5 +11,11 @@ def random_string(length):
         result.append(random.Random().choice(string.ascii_letters + string.digits))
     return "".join(result)
 
-with open("current_time.js", "w") as f:
-    f.write("var current_time="+str(millis)+";\nvar a=\""+(random_string(2**21))+"\";")
+with open("subdir/current_time.js", "w") as f:
+    f.write("current_time="+str(millis)+";\nvar random_filler=\""+(random_string(2**11))+"\";")
+
+
+for i in range(100):
+    with open("subdir/"+str(i)+".js", "w") as f:
+        f.write("var random_filler=\""+(random_string(2**11))+"\";")
+
